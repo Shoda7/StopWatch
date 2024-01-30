afccdea190fbf7d76b60d1d96c2578822d6a94e4
Shoda7 <matheus.shoda21@gmail.com>
2024-01-30 17:28:04 -0300

    inital commit

--- obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
+++ obj/Debug/net8.0/StopWatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+317864a9032ac6705f972dec376bf4d287e855e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StopWatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
